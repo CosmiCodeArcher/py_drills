d66cb888b23e319efa447c3b17f39ab6e4a6d0f7
Genenu <gackmar@outlook.com>
2026-08-09 18:37:52 +0100

    Check code for comments(the following commit message will be more like a journal). -- I came to the conclusion of this short and clean version after going through multiple trials and errors in my head, i could have gotten a working version that wasn't this clean if some of the failures i met midway which would have been unnecessary to the working version didn't come out of my uncertainty or naivety or curiosity.

---
+++ 5_password_strength.py
@@ -0,0 +1,31 @@
+# Implement password_strength(password). 
+# Return Weak if the password has fewer than 8 characters. 
+# Return Medium if it has at least 8 characters but does not contain both letters and digits. 
+# Return Strong if it has at least 8 characters and contains at least one letter and at least one digit. 
+# Students may need to research isalpha and isdigit.
+
+def password_strength(password):
+    if len(password) < 8:
+        return "Weak"
+
+    seen_alpha = False
+    seen_digit = False
+
+    for char in password:
+        if char.isalpha():
+            seen_alpha = True
+        elif char.isdigit():
+            seen_digit = True
+
+    # If both booleans result in True, password will always be "Strong"
+    if seen_alpha == True and seen_digit == True:
+        return "Strong"
+    else:
+        # When the guard clause for "Weak" password does not fire and --
+        # -- both booleans aren't true at the same time (which should result in strong) --
+        # -- The password would therefore be "Medium" since it's neither "Weak" nor "Strong"
+        return "Medium"
+
+print(password_strength("abc12345"))
+print(password_strength("abcdefgh"))
+print(password_strength("abc12"))
